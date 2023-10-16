c416c7967e6dcc0a40c08d75c56d82c3b60c2834
AlexiaMarin <101999457+dis0400@users.noreply.github.com>
2023-10-16 12:30:38 -0700

    Add files via upload

---
+++ WebUITesting/src/test/java/tarea/LogingTodoist.java
@@ -0,0 +1,34 @@
+package tarea;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.Homepage;
+import pages.LoginSection2;
+import session.Session;
+
+public class LogingTodoist {
+    LoginSection2 loginSection2 = new LoginSection2();
+    Homepage homepage = new Homepage();
+
+    @AfterEach
+    public void close(){
+        Session.getInstance().closeSession();
+    }
+    @BeforeEach
+    public void open(){
+        Session.getInstance().getBrowser().get("https://todoist.com/auth/login");
+    }
+    @Test
+    public void loginTesting(){
+        loginSection2.emailTextBox.setText("brendaaa@gmail.com");
+        loginSection2.pwdTextBox.setText("szdrwgwsrdfgvws");
+        loginSection2.loginButton.click();
+
+        Assertions.assertTrue(homepage.todayTitle.isControlDisplayed(),
+                "ERROR no me pude iniciar sesion");
+
+    }
+
+}
